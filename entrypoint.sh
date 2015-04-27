59218d156031a9d64888db6e2ba04d227d9bf80b
ArnaudPiroelle <piroelle.arnaud@gmail.com>
2015-04-27 15:21:26 +0200

    Change user used for connection

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+if [ -z "$SSH_USER" ]; then
+	echo "No ssh user defined. Exiting."
+    exit 1	
+fi
+
 if [ -z "$SSH_PASSWORD" ]; then
 	echo "No ssh password defined. Exiting."
     exit 1	
 fi
 
+
 mkdir /var/run/sshd
 
-echo "root:$SSH_PASSWORD" | chpasswd
+if [ $(getent passwd $SSH_USER | wc -l) -eq 0 ]; then
+	useradd --shell /sbin/nologin --no-create-home -G sftpusers $SSH_USER
+fi
 
-usermod -a -G sftpusers root
+echo "$SSH_USER:$SSH_PASSWORD" | chpasswd
 
 rsyslogd
 update-locale

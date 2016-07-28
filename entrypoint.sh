69b09738a44e0e3ab7678adbb5c98621e134d83c
Arnaud Piroelle <piroelle.arnaud@gmail.com>
2016-07-28 10:21:58 +0200

    Check if folder exist

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,17 @@
 
 if [ -z "$SSH_USER" ]; then
 	echo "No ssh user defined. Exiting."
-    exit 1	
+    exit 1
 fi
 
 if [ -z "$SSH_PASSWORD" ]; then
 	echo "No ssh password defined. Exiting."
-    exit 1	
+    exit 1
 fi
 
-
-mkdir /var/run/sshd
+if [ ! -d "/var/run/sshd" ]; then
+	mkdir /var/run/sshd
+fi
 
 if [ $(getent passwd $SSH_USER | wc -l) -eq 0 ]; then
 	useradd --shell /sbin/nologin --no-create-home -G sftpusers $SSH_USER
